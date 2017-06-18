cd391efee02314522f22c70b413b2e93f2630cf2
Tatiana Bercht <tatianabercht@tatianas-mbp.home>
2017-06-18 19:11:13 +0100

    empty working application with httpster

--- src/index.js
+++ src/index.js
@@ -1,8 +1,5 @@
 import React from 'react'
 import { render } from 'react-dom'
-import Menu from './components/Menu'
-import data from '../data/recipes.json'
-import './stylesheets/Menu.css'
 
 window.React = React
 
