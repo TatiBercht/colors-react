e88f24bf52af5dc5074dfdab916c97091e25003c
Tatiana Bercht <tatianabercht@tatianas-mbp.home>
2017-06-18 22:45:32 +0100

    minor edit to test

--- __tests__/components/ui/Color.test.js
+++ __tests__/components/ui/Color.test.js
@@ -4,10 +4,9 @@ import { compose } from 'redux'
 import Color from '../../../src/components/ui/Color'
 
 describe("<Color/> UI Component", () => {
-    const shallowExpect = compose(expect,toJSON,shallow)
 
     it("Renders correct properties", () =>
-        shallowExpect(
+        compose(expect,toJSON,shallow)(
             <Color title="Test Color" color="#F0F0F0" rating={3}  timestamp="Mon Apr 11 2016 12:54:19 GMT-0700 (PDT)" />
         ).toMatchSnapshot()
     )
