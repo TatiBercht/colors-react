ad8fc16b2de7b0a73f10b9190acba380dfa8d8bf
Tatiana Bercht <tatianabercht@tatianas-mbp.home>
2017-06-18 20:14:42 +0100

    app test and containers js

---
+++ __tests__/components/App.test.js
@@ -0,0 +1,9 @@
+import App from '../../src/components/App'
+
+jest.mock('../../src/components/containers')
+
+describe('<App/> Root component', () => {
+    it('renders correctly', () => {
+        expect(App().props.children.length).toBe(3)
+    })
+})
